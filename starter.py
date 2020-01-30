e628132abc40ed2fdbc6f2fe0fcfe0b7c56c25fe
sergencansiz <sergencansiz@gmail.com>
2020-01-30 17:56:53 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from operator import add
+import time
 
 sc = SparkContext("local" ,    "First App")
 
@@ -87,7 +88,6 @@ print('\n')
 ## Remove If the Values are same in pair RDD's such as (['x2'] , ['x2'])
 tmp = []
 def deneme(record):
-    
     x1 = record[0]
     x2 = record[1]
     
@@ -104,9 +104,10 @@ def deneme(record):
 
 def deneme2(record):
 
-    x1 = record[0]
-    x2 = record[1]
-    
+    if(any(lem == record[1] for lem in record[0]) == False):
+        return record[0][0] , record[0][1] , record[1] 
+    else:
+        return record[0]
 
 
 
@@ -142,34 +143,117 @@ thirdOne = newMap.cartesian(uniquesKeys)
 print(thirdOne.collect())
 print('\n')
 
-
-#print(csupport.collect())
-
-#print(secondOne.keys().collect())
-#print(secondOne.distinct().collect())
-
-
-#print(newOne.collect())
-#print(newOne.count())
-#print(newOne.distinct().count())
-
-# secondOneCombined = newOne.cartesian(filteredRecords)
-# secondOne = secondOneCombined.map(lambda record: deneme(record))
-# secondOne = secondOne.filter(lambda x : len(x) == 2)
-# print('------- Second Combined')
-# print(secondOneCombined.collect())
-# print('\n')
-
-# print('---- Second Remove Replica ------')
-# print(secondOne.collect())
-# print('----- \n')
+forthOne = thirdOne.map(lambda x: deneme2(x))
+forthOne = forthOne.filter(lambda x: len(x) > 2)
+print(forthOne.collect())
+print('\n')
 
 
+forthOne = forthOne.cartesian(starter)
+forthOne = forthOne.filter(lambda x: all(val in x[1] for val in x[0]))
+forthOne = forthOne.map(lambda x: (x[0],1))
+forthOne = forthOne.reduceByKey(sumOparator)
+forthOne = forthOne.filter(lambda x: x[1] > 1)
+#forthOne = forthOne.map(lambda)
 
-# print('----- COntrol -------')
-# print(secondOne.map(lambda x : len(x)).collect())
-# print(secondOne.collect())
-# print(newOne.collect())
+print(forthOne.collect())
+print('---\n')
+print(forthOne.isEmpty())
+print('---\n')
 
-# while(combinedRecords.isEmpty() == False):
+
+#### -----------------------------------------------------------------------------------------
+
+
+def removeReplica(record):
+
+    if(isinstance(record[0], tuple)):
+        x1 = record[0]
+        x2 = record[1]
+    else:
+        x1 = [record[0]]
+        x2 = record[1]
+
+    if(any(x == x2 for x in x1) == False):
+        a = list(x1)
+        a.append(x2)
+        a.sort()
+        result = tuple(a)
+        return result 
+    else:
+        return x1
+
+
+
+
+file =  sc.textFile("text.csv")
+
+## Whole Date set with frequencies  
+lblitems = file.map(lambda line: line.split(','))
+
+
+## Whole lines in single array 
+wlitems = file.flatMap(lambda line:line.split(','))
+
+## Unique frequent items in dataset
+uniqueItems = wlitems.distinct()
+
+
+supportRdd = wlitems.map(lambda item: (item , 1))
+supportRdd = supportRdd.reduceByKey(sumOparator)
+supports = supportRdd.map(lambda item: item[1])
+
+print('-------SUPOORTS---------------')
+print(supportRdd.collect())
+print('----\n')
+# Define minimum support value 
+minSupport = supports.min()
+
+# If mininmum support is 1 then replace it with 2 
+minSupport = 1 if minSupport == 1 else minSupport
+
+## Filter first supportRdd with minimum support 
+supportRdd = supportRdd.filter(lambda item: item[1] >= minSupport )
+
+## Craete base RDD with will be updated every iteration
+baseRdd = supportRdd
+
+supportRdd = supportRdd.map(lambda item: item[0])
+supportRddCart = supportRdd
+
+print(supportRdd.collect())
+
+print(minSupport)
+
+c = 2
+
+while(supportRdd.isEmpty() == False):
+
+    combined = supportRdd.cartesian(uniqueItems)
+    combined = combined.map(lambda item: removeReplica(item))
+  
+    combined = combined.filter(lambda item: len(item) == c)
+    combined = combined.distinct()
+
+    
+    combined_2 = combined.cartesian(lblitems)
+    combined_2 = combined_2.filter(lambda item: all(x in item[1] for x in item[0]))
+    
+    combined_2 = combined_2.map(lambda item: item[0])
+    combined_2 = combined_2.map(lambda item: (item , 1))
+    combined_2 = combined_2.reduceByKey(sumOparator)
+    combined_2 = combined_2.filter(lambda item: item[1] >= minSupport)
+    baseRdd = baseRdd.union(combined_2)
+    
+    combined_2 = combined_2.map(lambda item: item[0])
+    supportRdd = combined_2
+    print('-----RESUTLS-------')
+    print(combined_2.collect())
+    print('\n')
+    time.sleep(2)
+    c = c+1 
+
+print(baseRdd.collect())
+
+### Confidence Calculation 
 
